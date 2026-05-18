0645cb36bf1acebe73364a1858d7ef9a3da8c2a4
kate <142743629+perpetualcacophony@users.noreply.github.com>
2026-05-18 10:34:37 -0700

    guildMemberAdd listener is the money shot but idk how to work these listeners. messageCreate listener will tell us real quick when we figure it out

---
+++ src/listeners/guildMemberAdd.ts
@@ -0,0 +1,18 @@
+import { Listener } from "@sapphire/framework";
+import { GuildMember } from "discord.js";
+
+class GuildMemberAddListener extends Listener {
+    public constructor(context: Listener.LoaderContext, options: Listener.Options) {
+        super(context, {
+            ...options,
+            once: true,
+            event: 'guildMemberAdd'
+        });
+    }
+
+    run(member: GuildMember) {
+
+    }
+}
+
+module.exports = { GuildMemberAddListener };
---
+++ src/listeners/messageCreate.ts
@@ -0,0 +1,18 @@
+import { Listener } from "@sapphire/framework";
+import { Message } from "discord.js";
+
+class MessageCreateListener extends Listener {
+    public constructor(context: Listener.LoaderContext, options: Listener.Options) {
+        super(context, {
+            ...options,
+            once: true,
+            event: 'messageCreate'
+        });
+    }
+
+    async run(message: Message) {
+        await message.reply("testing, sorry!");
+    }
+}
+
+module.exports = { MessageCreateListener };
